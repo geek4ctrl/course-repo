bf9c14e8f401ef1d4415189e2eac9c979a83b67d
Laurent Mbuyu <LMbuyu@osiristrading.com>
2023-05-03 21:41:42 +0200

    [Refactor]: fizzbuzz takes numbers between 1 and 100.

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,6 +1,8 @@
 
 export function fizzBuzz(number: number) {
-    if (number % 15 == 0) return 'FizzBuzz'
-    if (number % 3 == 0) return 'Fizz'
-    if (number % 5 == 0) return 'Buzz'
+    if (number > 0 && number < 101) {
+        if (number % 15 == 0) return 'FizzBuzz'
+        if (number % 3 == 0) return 'Fizz'
+        if (number % 5 == 0) return 'Buzz'
+    }
 }
