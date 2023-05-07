117d4fc42744791eea5704972bcf7e5b1518fe4c
Laurent Mbuyu <LMbuyu@osiristrading.com>
2023-05-07 22:19:23 +0200

    [New]: Stats calculator returns an IResult object.

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,5 +1,19 @@
+import { IResult } from './index';
+import { statsCalculator } from './index';
 
 describe('stats calculator', () => {
 
+    it('should return an IResult object', () => {
+        // Arrange
+        let response: IResult;
 
-})
+        // Act
+        response = statsCalculator([2, 6, -1])
+
+        // Assert
+        expect(response.minimumValue).toBeFalsy();
+        expect(response.maximumValue).toBeFalsy();
+        expect(response.averageValue).toBeFalsy();
+
+    });
+});
---
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
@@ -0,0 +1,13 @@
+export interface IResult {
+    minimumValue: number,
+    maximumValue: number,
+    averageValue: number
+}
+
+export function statsCalculator(values: any): IResult {
+
+    let result = {} as IResult;
+
+    return result;
+
+}
