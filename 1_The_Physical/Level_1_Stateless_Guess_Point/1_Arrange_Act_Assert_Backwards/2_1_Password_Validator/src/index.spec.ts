b6601c5c59baad9f9268b7f99d0cdc544b7f3d6e
Laurent Mbuyu <LMbuyu@osiristrading.com>
2023-05-07 16:30:18 +0200

    [New]: Password validator should return an invalid length error for strings less than 5 characters.

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -1,4 +1,5 @@
-import { passwordValidator } from './index'
+import { passwordValidator } from './index';
+import { IResult } from './index'
 
 describe('password validator', () => {
 
@@ -8,6 +9,19 @@ describe('password validator', () => {
 
   it('should return an IResult object', () => {
     expect(passwordValidator("testing")).toBeTruthy()
+  })
+
+  it('should return an invalid length error for strings less than 5 characters.', () => {
+    // arrange
+    let response: IResult;
+
+    // act
+    response = passwordValidator('mom')
+
+    // assert
+    expect(response.result).toBeFalsy();
+    expect(response.errors.length).toEqual(1);
+    expect(response.errors[0].error).toContain('String length should be between 5 and 15 characters long.')
   })
 
 })
--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -1,19 +1,28 @@
-interface IResult {
+export interface IResult {
     result: boolean,
     errors: [
         {
-            textLength: 'String length should be between 5 and 15 characters long.'
+            error: string
         },
-        {
-            oneDigit: 'String should contain at least one digit.'
-        },
-        {
-            upperCase: 'String should contain at least one upper case letter.'
-        },
+
     ]
 }
 
 export function passwordValidator(str: string): IResult {
+
+    // const text = str.search(/^[A-Z]{5,15}/i);
     const result = {} as IResult;
+
+    if (str.length < 5 || str.length > 15) {
+        return {
+            result: false,
+            errors: [
+                {
+                    error: 'String length should be between 5 and 15 characters long.'
+                },
+            ]
+        }
+    }
+
     return result;
 }
