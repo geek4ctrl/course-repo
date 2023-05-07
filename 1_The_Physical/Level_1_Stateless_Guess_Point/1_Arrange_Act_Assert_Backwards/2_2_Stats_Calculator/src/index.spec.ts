aa664d633fb7fb5447741a99dc59d10f7956839b
Laurent Mbuyu <LMbuyu@osiristrading.com>
2023-05-07 23:08:03 +0200

    [New]: Stats calculator returns an average of 18.666666666666668.

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -13,7 +13,7 @@ describe('stats calculator', () => {
         // Assert
         expect(response.minimumValue).toBeTruthy();
         expect(response.maximumValue).toBeTruthy();
-        expect(response.averageValue).toBeFalsy();
+        expect(response.averageValue).toBeTruthy();
 
     });
 
@@ -37,5 +37,16 @@ describe('stats calculator', () => {
 
         // Assert
         expect(response.maximumValue).toEqual(53);
+    })
+
+    it('should return an average value of 18.666666666666668', () => {
+        // Arrange
+        let response: IResult;
+
+        // Act
+        response = statsCalculator([2, 4, 21, -8, 53, 40])
+
+        // Assert
+        expect(response.averageValue).toEqual(18.666666666666668);
     })
 });
--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
@@ -9,6 +9,7 @@ export function statsCalculator(values: any): IResult {
     let result = {} as IResult;
     let minimumValue = Number.MAX_VALUE;
     let maximumValue = Number.MIN_VALUE;
+    let averageValue = 0;
 
     for (let i = 0; i < values.length; i++) {
         if (minimumValue > values[i]) {
@@ -18,10 +19,13 @@ export function statsCalculator(values: any): IResult {
         if (maximumValue < values[i]) {
             maximumValue = values[i];
         }
+
+        averageValue += values[i];
     }
 
     result.minimumValue = minimumValue;
     result.maximumValue = maximumValue;
+    result.averageValue = averageValue / values.length;
 
     return result;
 
