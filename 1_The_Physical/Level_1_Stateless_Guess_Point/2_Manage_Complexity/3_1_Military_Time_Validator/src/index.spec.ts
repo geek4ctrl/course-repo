8848421d3c411b74ead6d6a03eb85eb27caca31a
Laurent Mbuyu <LMbuyu@osiristrading.com>
2023-05-10 17:41:37 +0200

    [New]: Military time validator should check whether there is a - delimiter.

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -11,4 +11,13 @@ describe('military time validator', () => {
         expect(militaryTimeValidator("01:12 - 14:32")).toEqual(true);
     });
 
+    it('should check whether there is a - delimiter', () => {
+        // Arrange
+
+        // Act
+
+        // Assert
+        expect(militaryTimeValidator("01:12 - 14:32")).toEqual(true);
+    });
+
 })
--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,4 +1,12 @@
 
 export function militaryTimeValidator(militaryTime: string): boolean {
+
+    const slashDelimiter = "-";
+    const militaryTimeArray = militaryTime.split(slashDelimiter);
+
+    if (militaryTimeArray.length == 0) {
+        return false;
+    }
+
     return true;
 }
